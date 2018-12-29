d5bb02fcff4e3c9601c333bb6c7dca2862a4f512
Derek Van Vuuren <derek.vanvuuren@gmail.com>
2018-12-28 20:52:27 -0800

    Update ip_update.sh

--- ip_update.sh
+++ ip_update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-domain=$1
 set -o nounset
-sed -i "s/^(.*) IN A (.*)$/1 IN A $1/" /etc/bind/db.$domain
-sed -i "s/.*Serial$/ $(date +%Y%m%d%H) ; Serial/" /etc/bind/db.$domain
+sed -i "s/^(.*) IN A (.*)$/1 IN A $1/" /etc/bind/db.YourDomain
+sed -i "s/.*Serial$/ $(date +%Y%m%d%H) ; Serial/" /etc/bind/db.YourDomain
 /etc/init.d/bind9 restart
